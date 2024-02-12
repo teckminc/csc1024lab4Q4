28553dc0bf9e5b0963ee973014dd187d058b5861
teckminc <102782040+teckminc@users.noreply.github.com>
2024-02-12 16:24:36 +0800

    First Commit
    First Commit

---
+++ lab4q4.py
@@ -0,0 +1,63 @@
+'''
+Do not remove any text from these comments
+4.	Write a temperature conversion program between degree Celsius and degree
+    Fahrenheit, according to user selection. The following is the formula:
+
+Celsius = (Fahrenheit – 32) * 5/9
+Fahrenheit = (Celsius * 9/5) + 32
+
+• The temperature conversion program is expected to convert a range of
+  temperature measurements from a minimum value to a maximum value.
+• The program will only execute if the minimum temperature is smaller 
+  than or equal to the maximum temperature. Else, an error message should 
+  be prompted to the user and the program restart.
+• The program will only execute when the menu selection is valid. Else, 
+  xan error message should be prompted to the user and the program restart.
+• Write an iteration check if the user would like to run the program again.
+
+Screen output examples:
+
+Temperature Conversion Programme.
+[1] Convert Celsius to Fahrenheit.
+[2] Convert Fahrenheit to Celsius.
+Enter your selection, 1 or 2: 1
+Celsius (C) to Fahrenheit (F) Conversion
+Enter temperature in integer values only.
+Enter minimum temperature: 20
+Enter maximum temperature: 25
+20C = 68.0F
+21C = 69.8F
+22C = 71.6F
+23C = 73.4F
+24C = 75.2F
+25C = 77.0F
+Conversion Done!
+Do you want to run the program again? [Y/N]: Y
+
+Temperature Conversion Programme.
+[1] Convert Celsius to Fahrenheit.
+[2] Convert Fahrenheit to Celsius.
+Enter your selection, 1 or 2: 3
+ERROR: Invalid Selection!
+Do you want to run the program again? [Y/N]: Y
+
+Temperature Conversion Programme.
+[1] Convert Celsius to Fahrenheit.
+[2] Convert Fahrenheit to Celsius.
+Enter your selection, 1 or 2: 2
+Fahrenheit (F) to Celsius (C) Conversion
+Enter temperature in integer values only.
+Enter minimum temperature: 50
+Enter maximum temperature: 30
+ERROR: Invalid Input!
+Do you want to run the program again? [Y/N]: Y
+
+Functions to use: print(), input(), int(),str.upper()
+Operators: ==,<=, +, -, /, *, 
+Structures: while, if-else, if – elif -else
+'''
+def main():
+
+            
+    return 0
+
